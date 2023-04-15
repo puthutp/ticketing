c845218a29e283731271644a793fffab9e557cfb
Puthut Prabancono <puthut.prabancono@gmail.com>
2023-04-15 10:58:46 +0700

    add orders service

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,5 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose, { Collection } from 'mongoose';
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
 
