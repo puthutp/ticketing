edad7fc9272762e1b03c2618ad90678a592a773c
Puthut Prabancono <puthut.prabancono@gmail.com>
2023-04-17 17:30:42 +0700

    add listen oder events in ticket service

---
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,36 @@
+import { Listener, OrderCancelledEvent, Subjects } from '@pptix/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // If no ticket, throw error
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    // Unset order id
+    ticket.set({ orderId: undefined });
+    await ticket.save();
+
+    new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
+
+    // ack the message
+    msg.ack();
+  }
+}
