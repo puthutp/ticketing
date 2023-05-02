ad65873bbfac10a8ef662c6a5b70f72306c3ac64
Puthut Prabancono <puthut.prabancono@gmail.com>
2023-05-02 11:48:02 +0700

    finish client / frontend

--- client/components/header.js
+++ client/components/header.js
@@ -4,13 +4,15 @@ const Header = ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
     !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sell Tickets', href: '/tickets/new' },
+    currentUser && { label: 'My Orders', href: '/orders' },
     currentUser && { label: 'Sign Out', href: '/auth/signout' },
   ]
     .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => {
       return (
-        <li key={href} className='nav-item'>
-          <Link href={href} className='nav-link'>
+        <li key={href} className="nav-item">
+          <Link href={href} className="nav-link">
             {label}
           </Link>
         </li>
@@ -18,13 +20,13 @@ const Header = ({ currentUser }) => {
     });
 
   return (
-    <nav className='navbar navbar-light bg-light'>
-      <Link href='/' className='navbar-brand'>
+    <nav className="navbar navbar-light bg-light">
+      <Link href="/" className="navbar-brand">
         GitTix
       </Link>
 
-      <div className='d-flex justify-content-end'>
-        <ul className='nav d-flex align-items-center'>{links}</ul>
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -4,10 +4,13 @@ import { useState } from 'react';
 export default ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
-  const doRequest = async () => {
+  const doRequest = async (props = {}) => {
     try {
       setErrors(null);
-      const response = await axios[method](url, body);
+      const response = await axios[method](url, {
+        ...body,
+        ...props,
+      });
 
       if (onSuccess) {
         console.log('request success');
