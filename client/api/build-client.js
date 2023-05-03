2ebc8ffd885b2beff6215d18687e65dc147e1f43
Puthut Prabancono <puthut.prabancono@gmail.com>
2023-05-04 05:26:49 +0700

    update build client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'http://www.build-test.xyz/',
+      baseURL:
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers,
     });
   } else {
