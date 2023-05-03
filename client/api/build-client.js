d06fabb3703b78d56f3eabcedbc664477fd829aa
Puthut Prabancono <puthut.prabancono@gmail.com>
2023-05-03 18:22:49 +0700

    fix hostname

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'http://build-test.xyz',
+      baseURL: 'http://www.build-test.xyz',
       headers: req.headers,
     });
   } else {
